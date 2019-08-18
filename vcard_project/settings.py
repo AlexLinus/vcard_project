91cf5656568b8c78d178883fcf94a7b98612b7ec
AlexLinus <kelevra141993@gmail.com>
2019-08-18 15:38:13 +0600

    configured ckeditor how it should work

--- vcard_project/settings.py
+++ vcard_project/settings.py
@@ -134,9 +134,26 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'width': '100%',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline', 'TextColor', 'Undo'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink', 'Anchor'],
+            ['Image', 'Table', ],
+            ['RemoveFormat', 'SpecialChar'],
+            ['CodeSnippet', 'Source',],
+        ],
+        # в ckeditor идет пачка встроенных плагинов, которые нужно включить.
+        'extraPlugins': 'codesnippet',
+    }
+}
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kelevra1493@gmail.com'
-EMAIL_HOST_PASSWORD = '0555764505'
+EMAIL_HOST_PASSWORD = 'NONE'
